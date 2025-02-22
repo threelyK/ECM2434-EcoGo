ae7b4507b15af9cd1ed670037d0d0bcda553c015
threelyK <scmichael23@yahoo.com>
2025-02-22 18:45:45 +0000

    Added `cards/` to the app urls

--- src/ecoGo/urls.py
+++ src/ecoGo/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.user.urls')),
-    # path('cards/' include('apps.cards.urls'))
+    path('cards/', include('apps.cards.urls')),
 ]
