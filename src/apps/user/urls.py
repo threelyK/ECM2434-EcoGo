df264e63667a9a047999529a82bd12920916a4e1
William Baldwin <williamthebud@live.co.uk>
2025-02-22 11:37:48 +0000

    Configured appropriate URLs to properly point to views

--- src/apps/user/urls.py
+++ src/apps/user/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('login', views.user_login, name="login"),  # Takes the user to the login page
     path('homepage', views.homepage, name="homepage"),  # Takes the user to the home page if they are already logged in
     path('user-logout', views.user_logout, name="user-logout"),  # Logs the user out
+    path("user/inventory", views.inventory, name="user-inventory"), # Takes the user to their inventory
+    path("user/inventory/sellCard", views.sell_card, name="sell-card"), # Sells a specified user card
 ]
