02b32c2dd1b277846184c2743eb29a5ffb3b7fd9
MarkosBont <markos.bont@gmail.com>
2025-02-24 17:12:52 +0000

    All user tests are completed

--- src/apps/user/tests.py
+++ src/apps/user/tests.py
@@ -55,6 +55,60 @@ class AuthenticationViewsTest(TestCase):
             "password2": "newpass123",
         }
 
+    def test_user_login_get(self):
+        """
+        Ensuring that a GET request to the login page returns the actual login form.
+        """
+        response = self.client.get(self.login_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'user/login.html')
+        self.assertIn("loginform", response.context)
+
+    def test_user_login_post_valid(self):
+        """
+        Simulating a login action, with valid user details. Ensuring that the user is authenticated and then redirected to homepage.
+        """
+        data = {
+            "username": self.user_credentials["username"],
+            "password": self.user_credentials["password"],
+        }
+        response = self.client.post(self.login_url, data, follow=True)
+        self.assertEqual(response.redirect_chain[-1][0], self.homepage_url)  # A successful login should redirect the user to the homepage.
+        self.assertTrue(SESSION_KEY in self.client.session)  # The user should be authenticated, this is checked by the session key.
+
+    def test_user_login_post_invalid(self):
+        """
+        Simulating a login attempt, with INVALID details. The response should render the login page with errors, indicating wrong details.
+        """
+        # Data which is not assocciated with any user
+        data = {
+            "username": self.user_credentials["username"],
+            "password": "wrongpassword",
+        }
+        response = self.client.post(self.login_url, data)
+        self.assertEqual(response.status_code, 200)  # The login view should re-render the form with errors (status code 200).
+        self.assertTemplateUsed(response, 'user/login.html')  # Ensuring that the login template is re-rendered.
+        self.assertFalse(SESSION_KEY in self.client.session)  # Ensuring the user is not authenticated via the session key.
+
+    def test_homepage_requires_authentication(self):
+        """
+        Verifying that an unauthenticated user trying to access the homepage is redirected to the login page (e.g. they type /homepage in the URL).
+        """
+        response = self.client.get(self.homepage_url)
+        self.assertEqual(response.status_code, 302)  # Expecting a status code 302 which means that there is a redirect
+        self.assertIn(self.login_url, response.url)  # Verifying that the login URL is in the redirect chain, meaning the user is redirected to the login page.
+
+    def test_user_logout(self):
+        """
+        Testing that an authenticated user can log out and is automatically redirected to the homepage.
+        """
+        self.client.login(username=self.user_credentials["username"],
+                          password=self.user_credentials["password"])  # Logging in the user.
+        self.assertTrue(SESSION_KEY in self.client.session)  # Ensuring user is logged in.
+        response = self.client.get(self.logout_url, follow=True)
+        self.assertRedirects(response, self.landing_url)  # After the logout occurrs, the user should be redirected to the landing page.
+        self.assertFalse(SESSION_KEY in self.client.session)  # Ensuring the user is logged out.
+
 
 class UserDataTest(TestCase):
     """
