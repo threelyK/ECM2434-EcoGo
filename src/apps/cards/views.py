83f81baa14f526b2b5f5cad0c0afd50af8daae81
threelyK <scmichael23@yahoo.com>
2025-02-24 21:01:06 +0000

    Fixed 2.0 🙏🙏🪦

--- src/apps/cards/views.py
+++ src/apps/cards/views.py
@@ -5,19 +5,16 @@ from django.contrib.auth.decorators import login_required
 from apps.user.models import UserData
 from apps.cards.models import Card
 
+def get_cards_instance():
+    if not Card.objects.exists():
+        Card.objects.create(card_name="Vortex-9", image="/images/card_images/Vortex-9.png", card_desc="")
+        Card.objects.create(card_name="Hydronis", image="/images/card_images/Hydronis.webp", card_desc="")
+        Card.objects.create(card_name="Crudespawn", image="/images/card_images/Crudespawn.png", card_desc="")
 
-if len(Card.objects.all()) == 0:
-    Card.objects.create(card_name="Vortex-9", image="/images/card_images/Vortex-9.png", card_desc="")
-    Card.objects.create(card_name="Hydronis", image="/images/card_images/Hydronis.webp", card_desc="")
-    Card.objects.create(card_name="Crudespawn", image="/images/card_images/Crudespawn.png", card_desc="")
-
-    
-cards_instance = {
-    # Contains Card and frame number 0 = Blue, 1 = Black, Change frame to be included in card model
+    return ({# Contains Card and frame number 0 = Blue, 1 = Black, Change frame to be included in card model
     "vor": (Card.objects.get(card_name="Vortex-9"), 0), 
     "hyd": (Card.objects.get(card_name="Hydronis"), 0),
-    "cru": (Card.objects.get(card_name="Crudespawn"), 1)
-}
+    "cru": (Card.objects.get(card_name="Crudespawn"), 1)})
 
 card_scan_UUIDs = {
     "vortex_UUIDs": ['4012cf77-7b46-4c2c-90f0-a1b821a123ea'],
@@ -43,7 +40,8 @@ def card_scan(request, url_UUID):
     Add specific card related to URL UUID into visitor's inventory
     """
     url_UUID = str(url_UUID)
-    
+    cards_instance = get_cards_instance()
+
     current_user = request.user
     current_UD = UserData.objects.get(owner=current_user)
 
