8a9a73930ab24810f0683ce99e71389c4448748b
threelyK <scmichael23@yahoo.com>
2025-02-24 18:55:20 +0000

    Updated views and settings

--- src/apps/cards/views.py
+++ src/apps/cards/views.py
@@ -15,22 +15,11 @@ try:
     if not Card.objects.get(card_name="Crudespawn"):                        
         Card.objects.create(card_name="Crudespawn", image="images/card_images/Crudespawn.", card_desc="")
 except:
-    Card.create_card(name="Vortex-9", 
-                 card_image="VORTEX-9.png", 
-                 desc="""Wind energy is one of the cheapest and fastest-growing renewable energy sources, 
-                 with modern turbines converting up to 50% of wind’s kinetic energy into electricity""")
-
-    Card.create_card(name="Hydronis", 
-                    card_image="Hydronis.webp", 
-                    desc="""Hydropower is the oldest form of mechanical renewable energy! 
-                    People have been using water to generate power for over 2,000 years, 
-                    dating back to ancient Greece, where water wheels were used to grind grain into flour!""")
-
-    Card.create_card(name="Crudespawn", 
-                        card_image="Crudespawn.png", 
-                        desc="""Oil drilling causes massive environmental damage, leading to oil spills, 
-                        habitat destruction, and water contamination. It also releases methane and carbon dioxide, 
-                        major contributors to climate change and air pollution, harming both ecosystems and human health.""")
+    Card.objects.create(card_name="Vortex-9", image="images/card_images/Vortex-9.png", card_desc="")
+                        
+    Card.objects.create(card_name="Hydronis", image="images/card_images/Hydronis.webp", card_desc="")
+                        
+    Card.objects.create(card_name="Crudespawn", image="images/card_images/Crudespawn.", card_desc="")
 
 
 
