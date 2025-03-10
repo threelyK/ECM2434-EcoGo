32fb57096a91df98157ab1e90b7f449c5b14367c
threelyK <scmichael23@yahoo.com>
2025-03-10 20:10:07 +0000

    Updates to `announcement_detail`, ready to be change with specific lat and long

--- src/apps/location/views.py
+++ src/apps/location/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from apps.qrgenerator.models import Website
+from random import uniform
 
 def announcement_list(request):
     websites = Website.objects.all()
@@ -21,11 +22,23 @@ def announcement_list(request):
 def announcement(request, slug):
     specific_announcement = get_object_or_404(Website, slug=slug)
 
-    exact_lat = specific_announcement.lat
-    exact_lon = specific_announcement.lon
-    # store centre of circle
+    #exact_lat = specific_announcement.lat
+    #exact_lon = specific_announcement.lon
+
+    # 25m = 0.000225 degrees lat
+    # +- 0.000135 degrees = ~15 m
+    offsetDeg = 0.000135
+
+    # circle_point_lat = exact_lat + uniform(-offsetDeg, offsetDeg)
+    # circle_point_lon = exact_lon + uniform(-offsetDeg, offsetDeg)
+
+    # Default origin point is set to Exeter
+    map_origin_point = [50.72552, -3.52689]
+
+    # circle_point = [circle_point_lat, circle_point_lon]
 
     view_context = {
+        "origin": map_origin_point,
         "radius": 25,
     }
 
