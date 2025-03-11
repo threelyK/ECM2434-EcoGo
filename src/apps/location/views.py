ecad840d2f2d0dcc33181e62952cc8c49a3e03e5
threelyK <scmichael23@yahoo.com>
2025-03-11 20:28:31 +0000

    Updated `announcement_list` and `announcement_detail` to include the latitude and longitude values

--- src/apps/location/views.py
+++ src/apps/location/views.py
@@ -8,9 +8,12 @@ def announcement_list(request):
     announcements = []
     for announcement in websites:
         announcements.append({
+            "id": announcement.id,
             "name": announcement.name,
             "url": announcement.url,
-            "location": announcement.location,
+            "date": announcement.date,
+            "lat": announcement.latitude,
+            "lon": announcement.longitude,
         })
 
     view_context = {
@@ -22,23 +25,20 @@ def announcement_list(request):
 def announcement(request, slug):
     specific_announcement = get_object_or_404(Website, slug=slug)
 
-    #exact_lat = specific_announcement.lat
-    #exact_lon = specific_announcement.lon
+    exact_lat = specific_announcement.latitude
+    exact_lon = specific_announcement.longitude
 
     # 25m = 0.000225 degrees lat
     # +- 0.000135 degrees = ~15 m
-    offsetDeg = 0.000135
+    offsetDeg = 0.00006
 
-    # circle_point_lat = exact_lat + uniform(-offsetDeg, offsetDeg)
-    # circle_point_lon = exact_lon + uniform(-offsetDeg, offsetDeg)
+    circle_point_lat = exact_lat + uniform((-1*offsetDeg), offsetDeg)
+    circle_point_lon = exact_lon + uniform((-1*offsetDeg), offsetDeg)
 
-    # Default origin point is set to Exeter
-    map_origin_point = [50.72552, -3.52689]
-
-    # circle_point = [circle_point_lat, circle_point_lon]
+    circle_point = [circle_point_lat, circle_point_lon]
 
     view_context = {
-        "origin": map_origin_point,
+        "circle_point": circle_point,
         "radius": 25,
     }
 
