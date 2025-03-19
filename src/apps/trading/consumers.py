d39dde196a9f400c9d27e35dd1b8da0879ea3ed7
William Baldwin <williamthebud@live.co.uk>
2025-03-19 00:35:07 +0000

    Ironed out some more bugs with the javascript

--- src/apps/trading/consumers.py
+++ src/apps/trading/consumers.py
@@ -20,14 +20,16 @@ class TradeConsumer(WebsocketConsumer):
 
         #State flag for inital joining of a room
         if data["state_flag"] == "J":
-            (room, response_owner) = get_room(rooms, data["room_name"])
+            self.room_name = data["room_name"]
+            (room, response_owner) = get_room(rooms, self.room_name)
             self.room = room
             response_func = get_response_func(room.room_owner, response_owner, self.user, self.send)
             room.join_room(self.user, response_func)
         #State flag for inital starting of a room
         elif data["state_flag"] == "S":
+            self.room_name = data["room_name"]
             room = TradingRoom(self.user)
-            rooms.append((data["room_name"], room, self.send))
+            rooms.append((self.room_name, room, self.send))
             self.room = room
             print(rooms)
 
@@ -35,11 +37,13 @@ class TradeConsumer(WebsocketConsumer):
             self.room.handle(data, self.user)
 
     def disconnect(self, code):
-        if (not self.room == None) and not code == 1000 and not self.room.state == "W": #Room exists and code is non-standard
+        if (not self.room is None) and not code == 1000 and not self.room.state == "W": #Room exists and code is non-standard
             self.room.disconnect(self.user)
 
-        if self.room.room_owner == self.user: #Only remove the room if you are user
-            rooms.remove(self.room)
+        if not self.room is None:
+            if self.room.room_owner == self.user: #Only remove the room if you are user
+                (room, func) = get_room(rooms, self.room_name)
+                rooms.remove((self.room_name, room, func))
 
 
         
--- src/ecoGo/static/js/trading.js
+++ src/ecoGo/static/js/trading.js
@@ -1,1 +1,23 @@
-console.log(user_type)
+socket.onopen = () => {
+    let message;
+
+    if(user_type == "owner"){
+        message = {
+            "state_flag": 'S',
+            "body":{},
+            "room_name": room_name
+        }
+    } else{
+        message = {
+            "state_flag": 'J',
+            "body":{},
+            "room_name": room_name
+        }
+    }
+
+    socket.send(JSON.stringify(message))
+}
+
+socket.onmessage = (event) =>{
+    console.log(event.data)
+}
