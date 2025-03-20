6202f43e7310e74a9426d8a683cb7e51557facee
RavingRaven <Thomas@Garratt.org.uk>
2025-03-20 14:06:58 +0000

    Minor changes to testing

--- src/apps/gamemaster/signals.py
+++ src/apps/gamemaster/signals.py
@@ -5,14 +5,18 @@ from django.db.models.signals import post_migrate
 from django.dispatch import receiver
 
 User = get_user_model()
-
 @receiver(post_migrate)
 def create_gamemaster_group(sender, **kwargs):
-    """Gamemaster Group creation on startup"""
+    """Ensure the Gamemaster group exists with the correct permissions"""
     gamemaster_group, created = Group.objects.get_or_create(name="Gamemaster")
 
     if created:
-        # Add view_user permission
+        print("Gamemaster group created!")
+        
         content_type = ContentType.objects.get_for_model(User)
-        permission = Permission.objects.get(codename='view_user', content_type=content_type)
-        gamemaster_group.permissions.add(permission)
+        try:
+            permission = Permission.objects.get(codename="view_user", content_type=content_type)
+            gamemaster_group.permissions.add(permission)
+            print("view_user permission added!")
+        except Permission.DoesNotExist:
+            print("view_user permission does not exist!")
