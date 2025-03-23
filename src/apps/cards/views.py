e62e3fc6b67cbabbfc727385c624eb1dbeabd9a7
threelyK <scmichael23@yahoo.com>
2025-03-23 20:43:12 +0000

    Replaced `Citygroup` to `Sustainable squad`

--- src/apps/cards/views.py
+++ src/apps/cards/views.py
@@ -36,7 +36,7 @@ def init_cards_instance():
 
 def init_pack_instance():
     """
-    Initializes 'citygroup' pack object. If the pack doesn't exist, it creates it.
+    Initializes 'Sustainable squad' pack object. If the pack doesn't exist, it creates it.
     """
 
     # Creates the rest of the card objects
@@ -48,40 +48,84 @@ def init_pack_instance():
     rea, created5 = Card.objects.get_or_create(card_name="REACT-O-TRON")
     the, created6 = Card.objects.get_or_create(card_name="Thermagon")
 
-    place_holder_text = "WIP"
-
     if created0:
         coa.image="/images/card_images/Coal-Imp.jpg"
-        coa.card_desc=place_holder_text
+        coa.card_desc="""
+        Coal mining devastates ecosystems, releasing 
+        toxic pollutants into the air and water. 
+        It contributes to climate change, destroys habitats, 
+        and leaves behind polluted land and waterways, creating 
+        environmental damage that lasts for generations.
+        """
         coa.save(update_fields=["image", "card_desc"])
     if created1:
         fun.image="/images/card_images/Funisprout.jpg"
-        fun.card_desc=place_holder_text
+        fun.card_desc="""
+        Biomass energy can reduce waste! By converting 
+        organic waste like agricultural leftovers, 
+        food scraps, and wood chips into energy, 
+        biomass helps reduce landfill waste and cuts 
+        down on methane emissions, which are harmful 
+        to the environment.
+        """
         fun.save(update_fields=["image", "card_desc"])
     if created2:
         hel.image="/images/card_images/HELIO-6.jpg"
-        hel.card_desc=place_holder_text
+        hel.card_desc="""
+        Solar panels can still generate electricity 
+        on cloudy days—they don’t need direct sunlight 
+        to work! While they produce the most energy in 
+        full sun, they can still capture diffused 
+        sunlight and operate at 10-25% efficiency on 
+        overcast days.
+        """
         hel.save(update_fields=["image", "card_desc"])
     if created3:
         liv.image="/images/card_images/Livewire.jpg"
-        liv.card_desc=place_holder_text
+        liv.card_desc="""
+        The power grid is one of the largest machines 
+        on Earth! It’s a huge, interconnected network 
+        spanning millions of miles of power lines, 
+        delivering electricity to billions of people 
+        in real time. Despite its size, it operates 
+        within a delicate balance—too much or too little 
+        power can cause blackouts.
+        """
         liv.save(update_fields=["image", "card_desc"])
     if created4:
         met.image="/images/card_images/Methanoth.jpg"
-        met.card_desc=place_holder_text
+        met.card_desc="""
+        Natural gas extraction can cause environmental 
+        damage through methane leaks, which are much 
+        more potent than carbon dioxide in trapping 
+        heat and contributing to climate change. Methane 
+        emissions from drilling and transportation can 
+        seep into the atmosphere, intensifying the global 
+        warming crisis.
+        """
         met.save(update_fields=["image", "card_desc"])
     if created5:
         rea.image="/images/card_images/REACT-O-TRON.jpg"
-        rea.card_desc=place_holder_text
+        rea.card_desc="""
+        Nuclear power plants produce zero carbon 
+        emissions while generating electricity! Unlike 
+        coal or gas plants, they don’t release greenhouse 
+        gases, making them one of the cleanest energy 
+        sources in terms of air pollution.
+        """
         rea.save(update_fields=["image", "card_desc"])
     if created6:
         the.image="/images/card_images/Thermagon.jpg"
-        the.card_desc=place_holder_text
-        rea.save(update_fields=["image", "card_desc"])
+        the.card_desc="""
+        Geothermal energy is virtually limitless! 
+        The heat from the Earth's core is constantly 
+        replenished, meaning we could theoretically harness 
+        geothermal power for billions of years without running out!
+        """
+        the.save(update_fields=["image", "card_desc"])
 
-
-    pack = Pack.objects.get_or_create(pack_name="Citygroup", cost=250, num_cards=10)[0]
-    if pack.get_all_cards().count() == 0:
+    pack, pack_created_1 = Pack.objects.get_or_create(pack_name="Sustainable squad", cost=250, num_cards=10, image="/images/pack_images/energy_pack.jpg")
+    if pack_created_1:
         pack.add_to_pack(Card.objects.get(card_name="Vortex-9"), 100)
         pack.add_to_pack(Card.objects.get(card_name="Hydronis"), 100)
         pack.add_to_pack(Card.objects.get(card_name="Crudespawn"), 100)
@@ -95,6 +139,227 @@ def init_pack_instance():
         if pack.validate_pack():
             pack.save_pack()
 
+    # For the Biobots
+    bio, created7 = Card.objects.get_or_create(card_name="Bio-Titan")
+    cor, created8 = Card.objects.get_or_create(card_name="Coral-Shield")
+    lum, created9 = Card.objects.get_or_create(card_name="Lumi-Pollinator")
+    ark, created10 = Card.objects.get_or_create(card_name="Ark-202X")
+    gai, created11 = Card.objects.get_or_create(card_name="Gaia-Filter")
+    ter, created12 = Card.objects.get_or_create(card_name="Terraform-X")
+    aer, created13 = Card.objects.get_or_create(card_name="Aero-Flock")
+    myc, created14 = Card.objects.get_or_create(card_name="Myco-Renew")
+    oce, created15 = Card.objects.get_or_create(card_name="Ocean-Sentinel")
+    gre, created16 = Card.objects.get_or_create(card_name="Green-Web")
+
+    if created7:
+        bio.image="/images/card_images/BIO-TITAN.jpeg"
+        bio.card_desc="""
+        Covering only 6% of Earth’s surface, rainforests 
+        support over 50% of all known species but are 
+        disappearing at a rate of 10 million hectares 
+        per year due to deforestation.
+        """
+        bio.save(update_fields=["image", "card_desc"])
+    if created8:
+        cor.image="/images/card_images/CORAL-SHIELD.jpeg"
+        cor.card_desc="""
+        Despite covering less than 1% of the ocean floor, 
+        coral reefs support 25% of marine life, yet over 
+        50% of reefs have experienced severe bleaching due 
+        to rising ocean temperatures.
+        """
+        cor.save(update_fields=["image", "card_desc"])
+    if created9:
+        lum.image="/images/card_images/LUMI-POLLINATOR.jpeg"
+        lum.card_desc="""
+        Pollinators contribute to 75% of global food 
+        crops, but habitat loss and pesticide use have 
+        led to a 40% decline in bee populations and 
+        threaten nocturnal pollinators like bats and moths.
+        """
+        lum.save(update_fields=["image", "card_desc"])
+    if created10:
+        ark.image="/images/card_images/ARK-202X.jpeg"
+        ark.card_desc="""
+        An estimated 40% of plant species are at risk of 
+        extinction due to habitat destruction, with seed 
+        banks storing over 1,000,000 species to preserve 
+        global biodiversity.
+        """
+        ark.save(update_fields=["image", "card_desc"])
+    if created11:
+        gai.image="/images/card_images/GAIA-FILTER.jpeg"
+        gai.card_desc="""
+        The ocean contains an estimated 14 million tons 
+        of microplastics, affecting marine life at all 
+        levels of the food chain and disrupting plankton 
+        populations, which generate over 50% of Earth’s oxygen.
+        """
+        gai.save(update_fields=["image", "card_desc"])
+    if created12:
+        ter.image="/images/card_images/TERRAFORM-X.jpeg"
+        ter.card_desc="""
+        Every year, 24 billion tons of fertile soil are 
+        lost due to deforestation and poor land management, 
+        essentially turning them into deserts, affecting over 
+        40% of the global population.
+        """
+        ter.save(update_fields=["image", "card_desc"])
+    if created13:
+        aer.image="/images/card_images/AERO-FLOCK.jpeg"
+        aer.card_desc="""
+        Light pollution and climate change disrupt the 
+        migration of 4,000 bird species, causing increased 
+        mortality due to collisions with buildings and 
+        communication towers.
+        """
+        aer.save(update_fields=["image", "card_desc"])
+    if created14:
+        myc.image="/images/card_images/MYCO-RENEW.jpeg"
+        myc.card_desc="""
+        Fungi decompose 90% of dead organic matter and 
+        can break down pollutants such as oil spills 
+        and heavy metals through a process known as 
+        mycoremediation.
+        """
+        myc.save(update_fields=["image", "card_desc"])
+    if created15:
+        oce.image="/images/card_images/OCEAN-SENTINEL.jpeg"
+        oce.card_desc="""
+        Over 80% of the ocean remains unexplored, yet 
+        deep-sea ecosystems host unique species and play 
+        a crucial role in regulating Earth’s climate and 
+        carbon cycles.
+        """
+        oce.save(update_fields=["image", "card_desc"])
+    if created16:
+        gre.image="/images/card_images/GREEN-WEB.jpeg"
+        gre.card_desc="""
+        Cities occupy only 3% of Earth’s land but generate 
+        75% of carbon emissions, while urban green spaces 
+        can reduce air pollution by up to 50% and support 
+        declining pollinator populations.
+        """
+        gre.save(update_fields=["image", "card_desc"])
+
+    pack2, pack_created_2 = Pack.objects.get_or_create(pack_name="Biobots", cost=250, num_cards=10, image="/images/pack_images/bd_pack_image.jpeg")
+    if pack_created_2:
+        pack2.add_to_pack(bio, 100)
+        pack2.add_to_pack(cor, 100)
+        pack2.add_to_pack(lum, 100)
+        pack2.add_to_pack(ark, 100)
+        pack2.add_to_pack(gai, 100)
+        pack2.add_to_pack(ter, 100)
+        pack2.add_to_pack(aer, 100)
+        pack2.add_to_pack(myc, 100)
+        pack2.add_to_pack(oce, 100)
+        pack2.add_to_pack(gre, 100)
+        if pack2.validate_pack():
+            pack2.save_pack()
+
+    # For the Waste warriors
+    pla, created17 = Card.objects.get_or_create(card_name="Plastic Pat")
+    rec, created18 = Card.objects.get_or_create(card_name="Recyc-Larry")
+    pap, created19 = Card.objects.get_or_create(card_name="Paper Pam")
+    ren, created20 = Card.objects.get_or_create(card_name="Renewox")
+    fer, created21 = Card.objects.get_or_create(card_name="Fertilis")
+    lan, created22 = Card.objects.get_or_create(card_name="Landfill Lewis")
+    rub, created23 = Card.objects.get_or_create(card_name="Rubbix")
+    pol, created24 = Card.objects.get_or_create(card_name="Pollutide")
+    haz, created25 = Card.objects.get_or_create(card_name="Hazmord")
+    lit, created26 = Card.objects.get_or_create(card_name="Litterimp")
+
+    if created17:
+        pla.image="/images/card_images/PlasticPat.jpg"
+        pla.card_desc="""
+        Only about 9% of all plastic ever produced has 
+        been recycled! The rest ends up in landfills, 
+        oceans or is incinerated.
+        """
+        pla.save(update_fields=["image", "card_desc"])
+    if created18:
+        rec.image="/images/card_images/Recyc-larry.jpg"
+        rec.card_desc="""
+        Recycling just one aluminium can saves enough 
+        energy to power a TV for 3 hours!
+        """
+        rec.save(update_fields=["image", "card_desc"])
+    if created19:
+        pap.image="/images/card_images/PaperPam.jpg"
+        pap.card_desc="""
+        Recycling just one ton of paper saves 17 trees, 
+        7000 gallons of water and enough energy to power 
+        a home for six months!
+        """
+        pap.save(update_fields=["image", "card_desc"])
+    if created20:
+        ren.image="/images/card_images/Renewox.jpg"
+        ren.card_desc="""
+        Using reusable bags instead of single-use plastic 
+        ones can save over 700 plastic bags per person per 
+        year.
+        """
+        ren.save(update_fields=["image", "card_desc"])
+    if created21:
+        fer.image="/images/card_images/Fertilis.jpg"
+        fer.card_desc="""
+        Composting just one banana peel creates enough 
+        nutrient-rich soil to grow several new banana 
+        plants!
+        """
+        fer.save(update_fields=["image", "card_desc"])
+    if created22:
+        lan.image="/images/card_images/LandfillLarry.jpg"
+        lan.card_desc="""
+        Landfills are responsible for releasing methane gas, 
+        a greenhouse gas that contributes to climate change.
+        """
+        lan.save(update_fields=["image", "card_desc"])
+    if created23:
+        rub.image="/images/card_images/Rubbix.jpg"
+        rub.card_desc="""
+        Many items tossed in black bins could be recycled or 
+        composted, but they're ofen sent to the wrong place, 
+        which worsens environmental damage.
+        """
+        rub.save(update_fields=["image", "card_desc"])
+    if created24:
+        pol.image="/images/card_images/Pollutide.jpg"
+        pol.card_desc="""
+        Over 8 million tons of plastic end up in the ocean 
+        every year, harming marine life and ecosystems.
+        """
+        pol.save(update_fields=["image", "card_desc"])
+    if created25:
+        haz.image="/images/card_images/Hazmord.jpg"
+        haz.card_desc="""
+        Hazardous waste can remain toxic for hundreds to 
+        thousands of years, contaminating soil, water, 
+        and air.
+        """
+        haz.save(update_fields=["image", "card_desc"])
+    if created26:
+        lit.image="/images/card_images/Litterimp.jpg"
+        lit.card_desc="""
+        Littering harms wildlife and the environment by 
+        causing entanglement, ingestion, and poisoning.
+        """
+        lit.save(update_fields=["image", "card_desc"])
+
+    pack3, pack_created_3 = Pack.objects.get_or_create(pack_name="Waste warriors", cost=250, num_cards=10, image="/images/pack_images/waste_warriors_pack.jpg")
+    if pack_created_3:
+        pack3.add_to_pack(pla, 100)
+        pack3.add_to_pack(rec, 100)
+        pack3.add_to_pack(pap, 100)
+        pack3.add_to_pack(ren, 100)
+        pack3.add_to_pack(fer, 100)
+        pack3.add_to_pack(lan, 100)
+        pack3.add_to_pack(rub, 100)
+        pack3.add_to_pack(pol, 100)
+        pack3.add_to_pack(haz, 100)
+        pack3.add_to_pack(lit, 100)
+        if pack3.validate_pack():
+            pack3.save_pack()
 
 card_scan_UUIDs = {
     "Vortex-9_UUIDs": ['4012cf77-7b46-4c2c-90f0-a1b821a123ea'],
@@ -103,7 +368,9 @@ card_scan_UUIDs = {
 }
 
 pack_scan_UUIDs = {
-    "Citygroup_UUIDs": ['8408d587-9b62-4d34-8dd7-4bfec213f443'],
+    "Sustainable squad_UUIDs": ['8408d587-9b62-4d34-8dd7-4bfec213f443'],
+    "Biobots_UUIDs": ['ab288e11-a71f-484c-99da-2c07c3237e0e'],
+    "Waste warriors_UUIDs": ['4a40b998-d445-488b-8aa2-8fdb2dc1d8c2'],
 }
 
 # A new entry will be created for new QR code with the repective index number
@@ -115,7 +382,9 @@ card_scan_visitors = {
 
 pack_scan_visitors = {
     # Dict cotaining userID and epoch time
-    "citygroup0_visitors": dict(),
+    "sustainable squad0_visitors": dict(),
+    "biobots0_visitors": dict(),
+    "waste warriors0_visitors": dict(),
 }
 
 # .===========.
